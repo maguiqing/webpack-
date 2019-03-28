0bb88b2dd552ebb73a9029ea94e44f95564fb9e0
maguiqing <v_maguiqing01@baidu.com>
2019-03-28 11:08:56 +0800

    初始化项目helloworld

--- webpack.config.js
+++ webpack.config.js
@@ -2,57 +2,72 @@ const path = require('path')
 const MiniCssExtractPlugin = require("mini-css-extract-plugin")
 const HtmlWebPackPlugin = require("html-webpack-plugin")
 const CleanWebpackPlugin = require('clean-webpack-plugin')
-module.exports = {
-    entry: [
+module.exports = (env, argv) => {
+    const devMode = argv.mode !== 'production'
+    return {
+      entry: [
         "babel-polyfill",
         path.join(__dirname, './src/index.js')
     ],
+    devServer: {
+        port: 3000, //端口号
+    },
     module: {
-      rules: [
-        {
-          test: /\.js$/,
-          exclude: /node_modules/,
-          use: {
-            loader: "babel-loader"
-          }
-        },
-        {
-            test: /\.css$/,
+        rules: [
+          {
+            test: /\.js$/,
+            exclude: /node_modules/,
+            use: {
+              loader: "babel-loader"
+            }
+          },
+          {
+            test: /\.less$/,
             use: [
                 devMode ? 'style-loader' : MiniCssExtractPlugin.loader,
                 'css-loader',
-                'postcss-loader'
+                'postcss-loader',
+                'less-loader',
             ]
-        },
-        {
-            test: /\.html$/,
-            use: [{
-                loader: "html-loader",
-                options: {
-                    minimize: true
+          },
+          {
+              test: /\.css$/,
+              use: [
+                  devMode ? 'style-loader' : MiniCssExtractPlugin.loader,
+                  'css-loader',
+                  'postcss-loader'
+              ]
+          },
+          {
+              test: /\.html$/,
+              use: [{
+                  loader: "html-loader",
+                  options: {
+                      minimize: true
+                  }
+              }]
+          },
+          {
+              test: /\.(png|jpg|gif)$/,
+              use: [
+                {
+                  loader: 'file-loader',
+                  options: {}
                 }
-            }]
-        },
-        {
-            test: /\.(png|jpg|gif)$/,
-            use: [
-              {
-                loader: 'file-loader',
-                options: {}
-              }
-            ]
-        }
-      ],
+              ]
+          }
+        ]
+      },
       plugins: [
         new MiniCssExtractPlugin({
             filename: "[name].css",
             chunkFilename: "[id].css"
         }),
         new HtmlWebPackPlugin({
-            template: "./src/index.html",
+            template: "./public/index.html",
             filename: "./index.html"
         }),
-        new CleanWebpackPlugin(['dist']),
+        new CleanWebpackPlugin()
       ]
     }
 }
